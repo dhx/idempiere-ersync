eff4bf4c7a6d20e19d68ca2302a59d7911d9de7b
Daniel Haag <git@dhx.at>
2016-04-17 09:59:56 +0200

    added ERSyncModelFactory and the service component to tell ADempiere about the Web Order Model.

---
+++ src/at/dhx/adempiere/ersync/model/ERSyncModelFactory.java
@@ -0,0 +1,35 @@
+package at.dhx.adempiere.ersync.model;
+
+import java.sql.ResultSet;
+
+import org.adempiere.base.IModelFactory;
+import org.compiere.model.PO;
+import org.compiere.util.Env;
+
+public class ERSyncModelFactory implements IModelFactory {
+
+	@Override
+	public Class<?> getClass(String tableName) {
+		if (tableName.equals(X_I_Web_Order.Table_Name)) {
+			return X_I_Web_Order.class;
+		}
+		return null;
+	}
+
+	@Override
+	public PO getPO(String tableName, int Record_ID, String trxName) {
+		if (tableName.equals(X_I_Web_Order.Table_Name)) {
+			return new X_I_Web_Order(Env.getCtx(), Record_ID, trxName);
+		}
+		return null;
+	}
+
+	@Override
+	public PO getPO(String tableName, ResultSet rs, String trxName) {
+		if (tableName.equals(X_I_Web_Order.Table_Name)) {
+			return new X_I_Web_Order(Env.getCtx(), rs, trxName);
+		}
+		return null;
+	}
+
+}
