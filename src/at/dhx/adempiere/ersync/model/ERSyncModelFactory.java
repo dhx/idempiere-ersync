835a36d4044866c8ee595b76d5ff33f6644637b0
Daniel Haag <git@dhx.at>
2016-05-09 17:33:54 +0200

    new version 1.3.0, new I_POS_Order Table

--- src/at/dhx/adempiere/ersync/model/ERSyncModelFactory.java
+++ src/at/dhx/adempiere/ersync/model/ERSyncModelFactory.java
@@ -12,6 +12,8 @@ public class ERSyncModelFactory implements IModelFactory {
 	public Class<?> getClass(String tableName) {
 		if (tableName.equals(X_I_Web_Order.Table_Name)) {
 			return X_I_Web_Order.class;
+		} else if (tableName.equals(X_I_POS_Order.Table_Name)) {
+			return X_I_POS_Order.class;
 		}
 		return null;
 	}
@@ -20,6 +22,8 @@ public class ERSyncModelFactory implements IModelFactory {
 	public PO getPO(String tableName, int Record_ID, String trxName) {
 		if (tableName.equals(X_I_Web_Order.Table_Name)) {
 			return new X_I_Web_Order(Env.getCtx(), Record_ID, trxName);
+		} else if (tableName.equals(X_I_POS_Order.Table_Name)) {
+			return new X_I_POS_Order(Env.getCtx(), Record_ID, trxName);
 		}
 		return null;
 	}
@@ -28,6 +32,8 @@ public class ERSyncModelFactory implements IModelFactory {
 	public PO getPO(String tableName, ResultSet rs, String trxName) {
 		if (tableName.equals(X_I_Web_Order.Table_Name)) {
 			return new X_I_Web_Order(Env.getCtx(), rs, trxName);
+		} else if (tableName.equals(X_I_POS_Order.Table_Name)) {
+			return new X_I_POS_Order(Env.getCtx(), rs, trxName);
 		}
 		return null;
 	}
